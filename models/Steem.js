5993c5b9ed32083d7368d3008203552d66f6662a
Maximiliaan van Dijk <meesvandijk0@gmail.com>
2018-01-24 20:18:59 +0100

    Default true for resteem func

--- models/Steem.js
+++ models/Steem.js
@@ -12,7 +12,7 @@ var Steem = new keystone.List('steem', {
 Steem.add({
 	username: { type: String, unique: true },
     	syncWith: { type: Types.Relationship, ref: 'Group', many: true},
-    	resteem: Boolean,
+    	resteem: {type: Boolean, default: true},
 	email: { type: Types.Email, unique: true },
 	password: { type: Types.Password },
     	wif: String,
